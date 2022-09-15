95e50ce9b8a4698c821d91ad69403032e7ca108b
dipti-sutradhar <107747892+dipti-sutradhar@users.noreply.github.com>
2022-09-15 12:55:35 +0600

    Add files via upload

---
+++ restock/js/main.js
@@ -0,0 +1,66 @@
+//active navbar
+
+//nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collape.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+//counter design
+// document.addEventListener("DOMContentLoaded",() => {
+// function counter(id, start, end, duration){
+//     let obj= document.getElementById(id),
+//     current = start,
+//     range = end - start,
+//     increment = end > start ? 1 : -1,
+//     step = Math.abs(Math.floor(duration / range)),
+//     timer = setInterval(() => {
+//         current += increment;
+//         obj.textContent = current;
+//         if(current == end){
+//             clearInterval(timer);
+//         }
+//     }, step);
+
+
+// counter("coun1", 0, 1287, 3000);
+// counter("coun2", 100, 5786, 2500);
+// counter("coun3", 0, 1440, 3000);
+// counter("coun4", 0, 7110, 3000);
+
+
+
+
+
+
+// });
+//counter design
+document.addEventListener("DOMContentLoaded", () => {
+  
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+     
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+            
+        }, step);
+    }
+    counter("coun1", 0, 1287, 3000);
+    counter("coun2", 100, 5786, 2500);
+    counter("coun3", 0, 1440, 3000);
+    counter("coun4", 0, 7110, 3000);
+
+
+});
